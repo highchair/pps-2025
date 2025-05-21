de905f2e67da4456645214050dd5d8ec30a18e90
J. Hogue <1530232+jhogue@users.noreply.github.com>
2025-05-20 20:13:46 -0400

    Fixed and added functions and a Header file

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
 
 // Adds theme support for post formats.
 if ( ! function_exists( 'pps_post_format_setup' ) ) :
-  function twentytwentyfive_post_format_setup() {
+  function pps_post_format_setup() {
     add_theme_support( 'post-formats', array( 'audio', 'chat', 'gallery', 'image', 'link', 'quote', 'status', 'video' ) );
   }
 endif;
@@ -13,7 +13,7 @@ add_action( 'after_setup_theme', 'pps_post_format_setup' );
 
 // Enqueues editor-style.css in the editors.
 if ( ! function_exists( 'pps_editor_style' ) ) :
-  function twentytwentyfive_editor_style() {
+  function pps_editor_style() {
     add_editor_style( get_parent_theme_file_uri( 'assets/css/editor-style.css' ) );
   }
 endif;
@@ -21,7 +21,7 @@ add_action( 'after_setup_theme', 'pps_editor_style' );
 
 // Enqueues style.css on the front.
 if ( ! function_exists( 'pps_enqueue_styles' ) ) :
-  function twentytwentyfive_enqueue_styles() {
+  function pps_enqueue_styles() {
     wp_enqueue_style(
       'twentytwentyfive-style',
       get_parent_theme_file_uri( 'style.css' ),
@@ -31,3 +31,30 @@ if ( ! function_exists( 'pps_enqueue_styles' ) ) :
   }
 endif;
 add_action( 'wp_enqueue_scripts', 'pps_enqueue_styles' );
+
+// Add persistent classes to the body element
+if ( ! function_exists( 'pps_body_classes' ) ) :
+  function pps_body_classes( $classes ) {
+    $classes[] = 'no-js';
+    $classes[] = 'not-scrolled';
+
+    // Optionally, add custom conditional classes
+    //if ( is_page('contact') ) {
+    //  $classes[] = 'contact-page-class';
+    //}
+
+    return $classes;
+  }
+endif;
+add_filter( 'body_class', 'pps_body_classes' );
+
+// Register menus
+if ( ! function_exists( 'pps_register_menus' ) ) :
+function pps_register_menus() {
+  register_nav_menus( array(
+    'utility'       => 'Utility Menu',
+    'primary'       => 'Primary Menu',
+  ) );
+}
+endif;
+add_action( 'after_setup_theme', 'pps_register_menus' );
---
+++ header.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package PPS 2025
+ */
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <link rel="preconnect" href="https://use.typekit.net">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <?php wp_head(); ?>
+
+    <link rel="stylesheet" href="https://use.typekit.net/msx3awg.css">
+  </head>
+  <body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+    
+    <div id="header" class="js__observer"></div>
+    <a href="#main" class="sr sr--focusable btn__secondary skip-link ">Skip to Content</a>
+    <header class="l__header header">
+      <div class="container header__wrap">
+        <div class="header__wrap__start">
+          <a class="brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <img class="brand__logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/pps-logo-circle.svg" alt="" />
+            <svg class="brand__wordmark" viewBox="0 0 170 66">
+              <title><?php bloginfo( 'name' ); ?></title>
+              <use xlink:href="#wordmark"></use>
+            </svg>
+          </a>
+        </div><!-- end .header__wrap__start -->
+        <button class="reset nav__trigger js__toggle" aria-controls="navContent" aria-haspopup="true" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="menu">&equiv;</span>
+          <span class="close">&#x2715;</span>
+          <span class="label">Menu</span>
+        </button>
+        <nav id="navContent" class="header__wrap__end nav" aria-label="Primary">
+          <div class="nav__inner">
+
+            <?php get_search_form(); ?>
+
+            <?php
+              $menu_primary = wp_nav_menu( array(
+                'theme_location' => 'primary',
+                'depth'			     => 2,
+                'menu_class'     => 'nav__main nav__list',
+                'container'      => 'ul',
+                'echo'           => FALSE,
+              ) );
+
+              if ( ! empty ( $menu_primary ) ) :
+                echo $menu_primary;
+              endif;
+
+              $menu_utility = wp_nav_menu( array(
+                'theme_location' => 'utility',
+                'depth'			     => 1,
+                'menu_class'     => 'nav__utility nav__list',
+                'container'      => 'ul',
+                'echo'           => FALSE,
+              ) );
+
+              if ( ! empty ( $menu_utility ) ) :
+                echo $menu_utility;
+              endif;
+            ?>
+
+          </div>
+        </nav>
+      </div><!-- end .header__wrap -->
+    </header>
