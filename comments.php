1594dfadd7476f0b4c4968ed320f40d3b6a2079a
J. Hogue <1530232+jhogue@users.noreply.github.com>
2025-05-20 20:13:24 -0400

    A working theme needs a comments file

---
+++ comments.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * The template for displaying comments
+ *
+ * This is the template that displays the area of the page that contains both the current comments
+ * and the comment form.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package PPS 2025
+ */
+
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+  return;
+}
+?>
+
+<div class="comments-area">
+
+  <?php
+  // You can start editing here -- including this comment!
+  if ( have_comments() ) :
+    ?>
+
+    <h2 class="comments-title heading-alt"><?php esc_html_e( 'Comments', 'life' ); ?></h2>
+
+    <?php the_comments_navigation(); ?>
+
+    <ol class="comment-list">
+      <?php
+      wp_list_comments(
+        array(
+          'short_ping' => true,
+        )
+      );
+      ?>
+    </ol><!-- .comment-list -->
+
+    <?php
+    the_comments_navigation();
+
+    // If comments are closed and there are comments, let's leave a little note, shall we?
+    if ( ! comments_open() ) :
+      ?>
+      <p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'life' ); ?></p>
+      <?php
+    endif;
+
+  endif; // Check for have_comments().
+
+  $custom_comment_field = '<p class="comment-form-comment"><textarea id="comment" name="comment" cols="45" rows="2" aria-required="true"></textarea></p>'; // label removed for cleaner layout
+
+  comment_form( array(
+    'comment_field'			=> $custom_comment_field,
+    'logged_in_as' 			=> '',
+    'comment_notes_before' 	=> '',
+    'title_reply_before' 	=> '<details><summary class="comment-reply-title">',
+    'title_reply_after' 	=> '</summary>',
+    'cancel_reply_after' 	=> '</details>',
+    'label_submit'			=> esc_html__('Submit Reply', 'life')
+  ));
+  ?>
+
+</div><!-- #comments -->
