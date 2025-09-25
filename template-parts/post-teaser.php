081559c2d3a0ff78021dbb36801e3bcac05e5c8b
J. Hogue <1530232+jhogue@users.noreply.github.com>
2025-09-25 09:19:13 -0400

    Update post tgeaser to handle Event content

--- template-parts/post-teaser.php
+++ template-parts/post-teaser.php
@@ -1,6 +1,7 @@
 <?php
 
   /* PPS template for news item (post) teasers */
+  /* Also used for Events custom post type teasers when using the Upcoming Events template (events-page.php) */
   /* Must be used inside a Wordpress while() loop */
   
   $classes = 'news-posts__item';
@@ -17,11 +18,21 @@
   }
 ?>
     <div class="news-posts__item__content">
-      <?php if ( 'events' == get_post_type() ) { ?>
-        <?php if ( get_field('event_date') ) { ?>
-          <p class="event-date pps__tag pps__tag--alt"><span role="time"><?php the_field('event_date'); ?></span></p>
-        <?php } ?>
-      <?php } ?>
+      <?php
+        if ( 'events' == get_post_type() ) {
+          $startdate = strtotime( get_post_meta( get_the_ID(), '_event_start_date', true ) );
+          $enddate = strtotime( get_post_meta( get_the_ID(), '_event_end_date', true ) );
+          if ( !empty( $startdate ) ) {
+            echo '<p class="event-date pps__tag pps__tag--alt">';
+            echo '<time class="event__start" datetime="'. date( 'Y-m-d', $startdate ) .'">'. date( 'F j, Y', $startdate ) .'</time>';
+            if ( !empty( $enddate ) ) {
+              echo '<span class="event__join"> through </span>';
+              echo '<time class="event__start" datetime="'. date( 'Y-m-d', $enddate ) .'">'. date( 'F j, Y', $enddate ) .'</time>';
+            }
+            echo '</p>';
+          }
+        }
+      ?>
       <h3 class="news-posts__item__title" style="margin-block-end: var(--wp--preset--spacing--sm)"><a class="news-posts__item__link" href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
       <p class="news-posts__item__excerpt">
         <?php echo get_the_excerpt() ?>
